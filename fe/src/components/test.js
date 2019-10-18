4647cf886d71e7684e68bc2b727fd36132f74180
Prince <arif.mahmud@surecash.net>
2019-10-18 12:02:41 +0600

    structurized UI test code

--- fe/src/components/test.js
+++ fe/src/components/test.js
@@ -2,25 +2,33 @@ require('chromedriver');
 const assert = require('assert');
 const {Builder, Key, By, until} = require('selenium-webdriver');
 
-describe('Check Format Money', function () {
+async function checkFormat(driver, numberVal) {
+        // Find the search box by id
+    await driver.findElement(By.id('prevValue')).sendKeys(numberVal);
+        // Enter keywords and click enter
+    await driver.findElement(By.id('submitBtn')).click();
+        // Wait for the results box by id
+    result = await driver.findElement(By.id('formattedValue')).getText();
+    await driver.findElement(By.id('prevValue')).clear();
+
+    return result;
+}
+
+describe('Check Format Money UI Test Case', function () {
     let driver;
     before(async function() {
         driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('http://localhost:3000');
     });
 
-    it('Format Money', async function() {
-        // Load the page
-        await driver.get('http://localhost:3000');
-        // Find the search box by id
-        await driver.findElement(By.id('prevValue')).sendKeys('9.999');
-        // Enter keywords and click enter
-        await driver.findElement(By.id('submitBtn')).click();
-        // Wait for the results box by id
-        let result = await driver.findElement(By.id('formattedValue')).getText();
-        // We will get the title value and test it
-        // let title = await driver.getTitle();
+    it('1. Check with maximum limit of digit value after decimal point', async function() {
+        result = await checkFormat(driver, '9.999');
         assert.equal(result, 'Formatted output: 10.00');
     });
+    it('2. Check with all zero before and after decimal point', async function() {
+        result = await checkFormat(driver, '0000.0000');
+        assert.equal(result, 'Formatted output: 0.00');
+    });
     // close the browser after running tests
-    after(() => driver && driver.quit());
+    //after(() => driver && driver.quit());
 })
